f9e4ac9902a7c559deda9d8ccae834377e11dc3b
MrAnnix <9783500+MrAnnix@users.noreply.github.com>
2026-03-18 09:38:02 +0100

    build: align compileSdk to 36 across Android modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.ecuforge.app"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.ecuforge.app"
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.ecuforge.core"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 26
